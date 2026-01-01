44508d670f47b4fae541ccd03bde3ffe1fa3afd3
sid <siddhaartha09@gmail.com>
2026-01-01 11:22:51 +0530

    prisma

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import type { Express, Response, Request } from 'express';
 
+dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Working');
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
