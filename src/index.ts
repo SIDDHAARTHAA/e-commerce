dc00315410a721d0e2f79b4b7adc9a3280710c1b
sid <siddhaartha09@gmail.com>
2026-01-01 11:33:24 +0530

    works

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import type { Express, Response, Request } from 'express';
+import rootRouter from './routes/index.js';
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req: Request, res: Response) => {
-    res.send('Working');
-})
+
+app.use('/api', rootRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
