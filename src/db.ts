82078ecc120c2b8740a7ced5b6e9ef8024942779
sid <siddhaartha09@gmail.com>
2026-01-01 12:27:14 +0530

    prisma setup

---
+++ src/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import pkg from "pg"
+
+const { Pool } = pkg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+})
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import type { Express, Response, Request } from 'express';
-import rootRouter from './routes/index.js';
+import "dotenv/config"
+import express from "express"
 
-dotenv.config();
-const app: Express = express();
-const port = process.env.PORT || 3000;
+import rootRouter from "./routes/index.js"
 
-app.use('/api', rootRouter);
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use("/api", rootRouter)
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-});
+  console.log(`Listening on port ${port}`)
+})
