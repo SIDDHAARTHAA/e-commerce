55a559254bd4919faa7d168275c7acd77b12d8d4
SIDDHAARTHAA <siddhaartha09@gmail.com>
2026-01-27 07:28:37 +0530

    product routes

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
 import { prisma } from "../db.js";
 import { createProductSchema } from "../schema/products.js";
+import { NotFoundException } from "../exceptions/not-found.js";
+import { ErrorCode } from "../exceptions/root.js";
 
 export const createProduct = async (req: Request, res: Response) => {
     const validatedData = createProductSchema.parse(req.body);
@@ -15,3 +17,77 @@ export const createProduct = async (req: Request, res: Response) => {
     })
     res.json(product)
 }
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const product = req.body;
+        if (product.tags) {
+            product.tags = product.tags.join(',');
+        }
+
+        const updatedProduct = await prisma.product.update({
+            where: {
+                id: +req.params.id!
+            },
+            data: product
+        })
+        res.json(updatedProduct)
+    } catch (error) {
+        throw new NotFoundException("Product not found", ErrorCode.PRODUCT_NOT_FOUND);
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(400).json({ error: "id required" });
+        return;
+    }
+    try {
+        const deletedProduct = await prisma.product.delete({
+            where: {
+                id: +req.params.id
+            },
+        })
+
+        res.json(deletedProduct);
+    } catch (error) {
+        throw new NotFoundException("Product not found", ErrorCode.PRODUCT_NOT_FOUND);
+    }
+}
+export const listProducts = async (req: Request, res: Response) => {
+    let skip;
+    if (!req.query.skip) {
+        skip = 0
+    } else {
+        skip = req.query.skip
+    }
+
+    const count = await prisma.product.count();
+
+    const products = await prisma.product.findMany({
+        skip: +skip,
+        take: 5
+    })
+
+    res.json({
+        count, data: products
+    })
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(400).json({ error: "id required" });
+        return;
+    }
+    try {
+        const product = await prisma.product.findFirstOrThrow({
+            where: {
+                id: +req.params.id
+            }
+        })
+        res.json(product);
+    } catch (error) {
+        throw new NotFoundException("Product not found", ErrorCode.PRODUCT_NOT_FOUND);
+    }
+}
+
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler.js";
-import { createProduct } from "../controllers/products.js";
+import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "../controllers/products.js";
 import authMiddleware from "../middlewares/auth.js";
 import adminMiddleware from "../middlewares/admin.js";
 
 const productsRoutes: Router = Router();
 
-productsRoutes.post('/', authMiddleware, adminMiddleware, errorHandler(createProduct))
+productsRoutes.post('/', authMiddleware, adminMiddleware, errorHandler(createProduct));
+productsRoutes.put('/:id', authMiddleware, adminMiddleware, errorHandler(updateProduct));
+productsRoutes.delete('/:id', authMiddleware, adminMiddleware, errorHandler(deleteProduct));
+productsRoutes.get('/', authMiddleware, adminMiddleware, errorHandler(listProducts));
+productsRoutes.get('/:id', authMiddleware, adminMiddleware, errorHandler(getProductById));
 
 export default productsRoutes;
