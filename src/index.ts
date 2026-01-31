42de0d48658d9a283206754440ff3adc455ce557
SIDDHAARTHAA <siddhaartha09@gmail.com>
2026-01-31 18:42:46 +0530

    fix: resovle cors issue blocking frontend requests

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,19 @@ import { errorMiddleware } from "./middlewares/errors.js"
 
 const app = express()
 app.use(express.json())
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://e-commerce-front-end.pages.dev",
+      "https://gear.sidlabs.shop"
+    ],
+    credentials: true,
+  })
+);
+
 app.use("/api", rootRouter)
 app.use(errorMiddleware)
 
